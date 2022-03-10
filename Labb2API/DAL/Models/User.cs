367723c6dcb7fa2c23cec613a387efd34f273cb1
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2022-03-10 16:49:23 +0100

    Created user record

--- Labb2API/DAL/Models/User.cs
+++ Labb2API/DAL/Models/User.cs
@@ -2,5 +2,21 @@
 {
     public record User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public List<Course> ActiveCourses { get; set; }
+
+        public User(string firstName, string lastName, string email, string phone, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            Address = address;
+            ActiveCourses = new List<Course>();
+        }
     }
 }
