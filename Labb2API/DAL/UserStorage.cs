a245c5189aedb99eabe29e83c7b942a4544b66d6
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2022-03-13 11:13:13 +0100

    Start adding HTML methods

--- Labb2API/DAL/UserStorage.cs
+++ Labb2API/DAL/UserStorage.cs
@@ -1,89 +1,66 @@
-﻿namespace Labb2API.DAL.Models
+﻿using Labb2API.DAL.Models;
+
+namespace Labb2API.DAL;
+
+public class UserStorage
 {
-    public class UserStorage
+    private readonly IDictionary<string, User> _users;
+
+    //private int _id;
+
+    public UserStorage()
     {
-        private readonly IDictionary<int, User> _users;
-
-        private int _id;
-
-        public UserStorage()
-        {
-            _users = new Dictionary<int, User>();
-        }
-
-        public bool CreateUser(User user)
-        {
-            if (_users.Values.Contains(user)) return false;
-
-            _users.Add(_id++, user);
-            return true;
-        }
-
-        public ICollection<User>? GetAllCourses()
-        {
-            return _users.Values;
-        }
-
-        //Kollar om det finns en user med en email, returnerar om den finns.
-        public User GetUser(string email)
-        {
-            foreach (var user in _users.Values)
-            {
-                if (user.Email == email) return user;
-            }
-
-            return null;
-        }
-
-        public bool UpdateUser(User user)
-        {
-            //TODO Scuffed sätt att hitta rätt keyvalue på.
-            var id = 0;
-
-            foreach (var u in _users.Values)
-            {
-                if (u == user)
-                {
-                    //TODO Kommer jag ut ur if-satsen eller foreachen?
-                    break;
-                }
-                id++;
-            }
-
-            if (!_users.Keys.Contains(id)) return false;
-            _users[id] = user;
-            return true;
-        }
-
-        //TODO Gör Add/Delete metoder istället för denna.
-        public bool UpdateActiveCourses(int id, List<Course> courses)
-        {
-            if (courses.Count <= 0) return false;
-
-            _users[id].ActiveCourses = courses;
-
-            return true;
-        }
-
-        public bool DeleteUser(User user)
-        {
-            //TODO Scuffed sätt att hitta rätt keyvalue på.
-            var id = 0;
-
-            foreach (var u in _users.Values)
-            {
-                if (u == user)
-                {
-                    //TODO Kommer jag ut ur if-satsen eller foreachen?
-                    break;
-                }
-                id++;
-            }
-
-            if (!_users.Keys.Contains(id)) return false;
-            _users[id] = user;
-            return true;
-        }
+        _users = new Dictionary<string, User>();
     }
-}
+
+    public bool CreateUser(User user)
+    {
+        if (_users.Keys.Contains(user.Email)) return false;
+
+        _users.Add(user.Email, user);
+
+        return true;
+    }
+
+    public ICollection<User>? GetAllUsers()
+    {
+        return _users.Values;
+    }
+
+    //Kollar om det finns en user med en email, returnerar om den finns.
+    public User? GetUser(string email)
+    {
+        if (_users.ContainsKey(email)) return _users[email];
+
+        return null;
+    }
+
+    //PUT? Ändrar hela användaren.
+    public bool UpdateUser(User user)
+    {
+        if (!_users.Keys.Contains(user.Email)) return false;
+
+        _users[user.Email] = user;
+
+        return true;
+    }
+
+    //TODO Gör Add/Delete metoder istället för denna.
+    public bool UpdateActiveCourses(string email, List<Course> courses)
+    {
+        if (courses.Count <= 0) return false;
+
+        _users[email].ActiveCourses = courses;
+
+        return true;
+    }
+
+    public bool DeleteUser(User user)
+    {
+        if (!_users.Keys.Contains(user.Email)) return false;
+
+        _users.Remove(user.Email);
+
+        return true;
+    }
 }
--- Labb2API/Program.cs
+++ Labb2API/Program.cs
@@ -1,6 +1,71 @@
+using Labb2API.DAL;
+using Labb2API.DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Denna gör att [FromServices] funkar, man lägger till en instans av en UserStorage.
+builder.Services.AddSingleton<UserStorage>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Testing!");
+
+//USER-relaterade metoder.
+
+//GET, hämta alla users.
+app.MapGet("/users", ([FromServices] UserStorage storage) =>
+{
+    var users = storage.GetAllUsers();
+
+    if (users.Count <= 0)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(users);
+});
+
+//GET, hämta en user
+app.MapGet("/users/{email}", ([FromServices] UserStorage storage, string email) =>
+{
+    var user = storage.GetUser(email);
+
+    if (user is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(user);
+});
+
+//POST, Skapa en user.
+app.MapPost("/users", ([FromServices] UserStorage storage, User user) =>
+{
+    if (string.IsNullOrEmpty(user.Email))
+    {
+        return Results.BadRequest();
+    }
+
+    if (storage.CreateUser(user))
+    {
+        return Results.Ok();
+    }
+    return Results.Conflict();
+});
+
+//PATCH/PUT, Uppdatera en user.
+
+//PATCH, uppdatera en users lista av aktiva kurser.
+
+//DELETE, ta bort en user.
+
+//COURSE-relaterade metoder.
+//GET, hämta alla courses.
+//GET, hämta en course.
+//POST, skapa en course.
+//PATCH/PUT, uppdatera en course.
+//PATCH, uppdatera status på en course
+//DELETE, ta bort en course.
 
 app.Run();
