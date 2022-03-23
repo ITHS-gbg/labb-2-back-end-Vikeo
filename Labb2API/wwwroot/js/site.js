7ae5b4907a0db3e244d09b5ea2f33ae6aa9518a6
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2022-03-23 21:01:24 +0100

    fix get users and their courses js

--- Labb2API/wwwroot/js/site.js
+++ Labb2API/wwwroot/js/site.js
@@ -3,7 +3,14 @@
 
 // Write your JavaScript code.
 
-
-
-
+class User {
+    constructor(firstName, lastName, email, phone, address, activeCourses) {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Email = email;
+        this.Phone = phone;
+        this.Address = address;
+        this.ActiveCourses = activeCourses;
+    }
+}
 
