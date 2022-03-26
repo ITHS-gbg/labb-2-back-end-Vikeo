204316ce2ec2f74973bee665d872766cb2d29657
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2022-03-26 17:51:49 +0100

    add ability to add/remove active course on EditUser page

--- Labb2API/Pages/EditUser.cshtml.cs
+++ Labb2API/Pages/EditUser.cshtml.cs
@@ -19,6 +19,11 @@ namespace Labb2API.Pages
         [BindProperty]
         public User SelectedUser { get; set; }
 
+        [BindProperty]
+        public List<Course> Courses { get; set; }
+        [BindProperty]
+        public int SelectedCourseId { get; set; }
+
         public async Task OnGetAsync()
         {
             var client = _httpClientFactory.CreateClient("api");
@@ -35,8 +40,24 @@ namespace Labb2API.Pages
                 Users = new List<User>();
                 Console.WriteLine(e);
             }
+
+            client = _httpClientFactory.CreateClient("api");
+            request = new HttpRequestMessage(HttpMethod.Get, "controller/courses");
+            response = await client.SendAsync(request);
+
+            content = await response.Content.ReadAsStreamAsync();
+            try
+            {
+                Courses = await JsonSerializer.DeserializeAsync<List<Course>>(content);
+            }
+            catch (Exception e)
+            {
+                Courses = new List<Course>();
+                Console.WriteLine(e);
+            }
         }
-        public async Task<IActionResult> OnPostAsync()
+
+        public async Task<IActionResult> OnPostUserAsync()
         {
             var body = JsonContent.Create(new
             {
@@ -51,6 +72,25 @@ namespace Labb2API.Pages
             var client = _httpClientFactory.CreateClient("api");
             var request = new HttpRequestMessage(HttpMethod.Put, $"controller/users/{SelectedUser.Email}");
             request.Content = body;
+            var response = await client.SendAsync(request);
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostCourseAsync()
+        {
+            var client = _httpClientFactory.CreateClient("api");
+            var request = new HttpRequestMessage(HttpMethod.Post, $"controller/users/{SelectedUser.Email}/{SelectedCourseId}");
+            var response = await client.SendAsync(request);
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync()
+        {
+            Request.Form["courseTitle"].ToString();
+            var client = _httpClientFactory.CreateClient("api");
+            var request = new HttpRequestMessage(HttpMethod.Delete, $"controller/users/{Request.Form["deleteCourseEmail"].ToString()}/{Request.Form["deleteCourseId"].ToString()}");
             var response = await client.SendAsync(request);
 
             return RedirectToPage();
